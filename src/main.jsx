63483d4c48d60f6313154a93beba2d6150a6e119
elias444 <elias@elogroup.net>
2025-07-16 19:02:18 -0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import RoloApp from './RoloApp.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RoloApp />
+);
